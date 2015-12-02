09567d77cc2128d421acc8b82ce86d7fc22685b6
Digvijay Makwana <digvijaymakwana@gmail.com>
2015-12-02 00:52:54 -0500

    basic foundation added

--- src/prm.cpp
+++ src/prm.cpp
@@ -15,45 +15,38 @@
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
 #include <Eigen/Dense>
 #include <math.h>
+#include <vector>
 
 using namespace Eigen;
 using geometry_msgs::Point;
+using geometry_msgs::PoseWithCovarianceStamped;
+using std::vector;
 
 ros::Publisher marker_pub;
 
 #define SIMULATION // change this to live if necessary
+
 #define TAGID 0
 #define COLS 100
 #define ROWS 100
-#define numSamples 10 //Number of Samples
+#define numMilestones 95 //Number of Samples
 #define nhDistance 10.0 //Neighborhood Distance
 #define obsThreshold 50
 
-int gMap[ROWS*COLS];
+vector<int8_t> gMap;
 bool initial_pose_found = false;
-
-vector<tuple<uint8_t,uint8_t,uint16_t>> milestones;
-tuple<uint8_t,uint8_t,uint16_t> startPoint;
-tuple<uint8_t,uint8_t,uint16_t> currPoint;
-vector<tuple<uint8_t,uint8_t,uint16_t>> wayPoints;
-
 vector<Point> milestones;
 Point startPoint;
 Point currPoint;
 vector<Point> wayPoints;
+vector<Point> path;
 
-
-int getGridVal(uint8_t x, uint8_t y) {
+int8_t getGridVal(uint8_t x, uint8_t y) {
 	return gMap[(y*10)*COLS + x*10];
 }
 
-int convertX(float64 x) {
-
-}
-
 // Used to display milestones & samples on RVIZ
-void draw_points(geometry_msgs::Point points_data[])
-{
+void draw_points(vector<Point> points_vector) {
 	visualization_msgs::Marker points;
 	points.header.frame_id = "/map";
 	points.ns = "Points";
@@ -65,105 +58,58 @@ void draw_points(geometry_msgs::Point points_data[])
 	points.scale.y = 0.2;
 	points.color.g = 1.0f;
 	points.color.a = 1.0;
-
-	for(int i = 0; i < numSamples; i++ ) {
-		points.points.push_back(points_data[i]);
-	}
-
+	points.points = points_vector;
 	marker_pub.publish(points);
 }
 
-
 // Euclidian Distance between 2 points
-double dist(geometry_msgs::Point p1,geometry_msgs::Point p2) {
+double dist(Point p1,Point p2) {
 	return pow(pow(p2.x - p1.x,2) + pow(p2.y - p1.y,2), 0.5);
 }
 
-
 // Generate the graph connections for each milestone
 // TODO: ensure milestones array passed in is correct
-void gen_connections(geometry_msgs::Point points[])
-{
-	int mS = 3;
-	// TODO: don't hardcode the size? Doesn't compile if a var is passed in...
-	Matrix<int, 3, 3> map;
-	map.fill(0);
+// void gen_connections(Point points[])
+// {
+// 	int mS = 3;
+// 	// TODO: don't hardcode the size? Doesn't compile if a var is passed in...
+// 	Matrix<int, 3, 3> map;
+// 	map.fill(0);
 
-	for(int i = 0; i < mS; i++) {
-		double norms[mS];
-		std::cout << points[i].x << std::endl;
+// 	for(int i = 0; i < mS; i++) {
+// 		double norms[mS];
+// 		std::cout << points[i].x << std::endl;
 
-		for (int j = 0; j < mS; j++) {
-			// Set neighbors based on proximity
-			double d = dist(points[i],points[j]);
-			if ( i != j && d > 0.0  && d < nhDistance) {
-				// TODO: collision check before adding edge
-				map(i,j) = 1;
-				map(j,i) = 1;
-			}
-		}
-	}
+// 		for (int j = 0; j < mS; j++) {
+// 			// Set neighbors based on proximity
+// 			double d = dist(points[i],points[j]);
+// 			if ( i != j && d > 0.0  && d < nhDistance) {
+// 				// TODO: collision check before adding edge
+// 				map(i,j) = 1;
+// 				map(j,i) = 1;
+// 			}
+// 		}
+// 	}
 
-	std::cout << map << std::endl;
-}
-
+// 	std::cout << map << std::endl;
+// }
 
 // Generate an array of milestones, display on RVIZ
-void gen_milestones()
-{
-		//Set up map bounds
-		// Vector2d xMax(9,5);
-		// Vector2d xMin(-1,-5);
-		// Vector2d xR;
-		// xR = xMax - xMin;
-		//
-		// // Set up goals
-		// Vector3d w1(5,5,0);
-		// Vector3d w2(1,0,3.14);
-		// Vector3d w3(1.5,4.5,-1.57);
-		// Vector3d w4(3,0.5,1.57);
-
-		// collect certain number of Samples with rand
-		// filter samples by checking map
-		// update milestons vector
-
-		// Generate sample points
-		Vector2d samples [numSamples];
-		geometry_msgs::Point p_data [numSamples];
-
-		for(int i = 0; i < numSamples; i++) {
-			double r1 = ((double) rand() / (RAND_MAX));
-			double r2 = ((double) rand() / (RAND_MAX));
-
-			// TODO: Vector to Point conversion?
-			//For RVIZ
-			Point p;
-			p.x = xR(0)*r1 + xMin(0);
-			p.y = xR(1)*r2 + xMin(1);
-			p.z = 0;
-			p_data[i] = p;
-
-			Vector2d s(xR(0)*r1 + xMin(0), xR(1)*r2 + xMin(1));
-			samples[i] = s;
-			// std::cout << s << std::endl;
+void gen_milestones() {	
+	while (milestones.size() < numMilestones) {
+		int x = rand() % COLS;
+		int y = rand() % ROWS;
+		int z = 0;
+		if (getGridVal(x,y) < obsThreshold) {
+			Point p; p.x = (double)x; p.y = (double)y; p.z = (double)z;
+			milestones.push_back(p);
 		}
-
-		Vector3d milestones [3];
-		milestones[0] = w1;
-    std::cout << milestones[0](0) << std::endl;
-
-		// TODO: select valid milestones
-		for (int i = 0; i < numSamples; i++) {
-			if(getGridVal(samples[i].x,samples[i].y) > obsThreshold) {
-
-			}
-		}
-		draw_points(p_data);
+	}
+	draw_points(milestones);
 }
 
-
 //Callback function for the Position topic (LIVE)
-void pose_callback(const geometry_msgs::PoseWithCovarianceStamped & msg)
+void pose_callback(const PoseWithCovarianceStamped &msg)
 {
 	//This function is called when a new position message is received
 	double X = msg.pose.pose.position.x; // Robot X psotition
@@ -177,7 +123,6 @@ void pose_callback(const geometry_msgs::PoseWithCovarianceStamped & msg)
 	initial_pose_found = true;
 	// std::cout << "X: " << X << ", Y: " << Y << ", Yaw: " << Yaw << std::endl ;
 }
-
 
 //Example of drawing a curve
 void drawCurve(int k)
@@ -202,7 +147,7 @@ void drawCurve(int k)
 
    //generate curve points
    for(int i = 0; i < steps; i++) {
-       geometry_msgs::Point p;
+       Point p;
        p.x = x;
        p.y = y;
        p.z = 0; //not used
@@ -218,8 +163,7 @@ void drawCurve(int k)
 
 }
 
-
-void drawLineSegment(int k, geometry_msgs::Point start_point, geometry_msgs::Point end_point)
+void drawLineSegment(int k, Point start_point, Point end_point)
 {
    visualization_msgs::Marker lines;
    lines.header.frame_id = "/map";
@@ -239,46 +183,77 @@ void drawLineSegment(int k, geometry_msgs::Point start_point, geometry_msgs::Poi
    marker_pub.publish(lines);
 }
 
-
 //Callback function for the map
 void map_callback(const nav_msgs::OccupancyGrid& msg) {
-		gMap = msg.data;
+	gMap = msg.data;
 }
 
+bool find_shortest_path() {
+	// TODO
+	return false;
+}
 
+void init_map() {
+    for (int i = 0; i < ROWS*COLS; i++) {
+        gMap.push_back(0);
+    }
+}
 
+void init_vectors() {
+	milestones.clear();
+	wayPoints.clear();
+	path.clear();
+
+	// For simulation - insert waypoints
+#ifdef SIMULATION
+	Point p1; p1.x = 4.0; p1.y = 0.0; p1.z = 0.0;
+	Point p2; p2.x = 8.0; p2.y = -4.0; p2.z = 3.14;
+	Point p3; p3.x = 8.0; p3.y = 0.0; p3.z = -1.57;
+	wayPoints.push_back(p1);
+	wayPoints.push_back(p2);
+	wayPoints.push_back(p3);
+	milestones.push_back(p1);
+	milestones.push_back(p2);
+	milestones.push_back(p3);
+#else
+	// For live
+	Point p1; p1.x = 5.0; p1.y = 5.0; p1.z = 0.0;
+	Point p2; p2.x = 1.0; p2.y = 0.0; p2.z = 3.14;
+	Point p3; p3.x = 1.5; p3.y = 4.5; p3.z = -1.57;
+	Point p4; p4.x = 3.0; p4.y = 0.5; p4.z = 1.57;
+	wayPoints.push_back(p1);
+	wayPoints.push_back(p2);
+	wayPoints.push_back(p3);
+	wayPoints.push_back(p4);
+	milestones.push_back(p1);
+	milestones.push_back(p2);
+	milestones.push_back(p3);
+	milestones.push_back(p4);
+#endif
+}
 
 int main(int argc, char **argv)
 {
 	//Initialize the ROS framework
     ros::init(argc,argv,"main_control");
     ros::NodeHandle n;
-
+    srand(time(NULL));
+    init_map();
     //Subscribe to the desired topics and assign callbacks
     ros::Subscriber map_sub = n.subscribe("/map", 1, map_callback);
     ros::Subscriber pose_sub = n.subscribe("/indoor_pos", 1, pose_callback);
-		ROS_INFO("Subscribed to everything! Waiting for initial pose.");
+	ROS_INFO("Subscribed to everything! Waiting for initial pose.");
 
-		ros::Rate loop_rate(20);    //20Hz update rate
-		while (!initial_pose_found) {
-			loop_rate.sleep();
-			ros::spinOnce();
-		}
-		ROS_INFO("Receieved initial pose!");
+	ros::Rate loop_rate(20);    //20Hz update rate
+	while (!initial_pose_found) {
+		loop_rate.sleep();
+		ros::spinOnce();
+	}
+	ROS_INFO("Receieved initial pose!");
 
-		// For simulation - insert waypoints
-	#ifdef SIMULATION
-		wayPoints.push_back(Point(4.0,0.0,0.0));
-		wayPoints.push_back(Point(8.0,-4.0,3.14));
-		wayPoints.push_back(Point(8.0.0,0.0,-1.57));
-	#else
-		// For live
-		wayPoints.push_back(Point(5.0,5.0,0.0));
-		wayPoints.push_back(Point(1.0,0.0,3.14));
-		wayPoints.push_back(Point(1.5,4.5,-1.57));
-		wayPoints.push_back(Point(3.0,0.5,1.57));
-	#end
+	wayPoints.push_back(startPoint);
 
+	ROS_INFO("Initial waypoints and milestones updated!");
 
     //Setup topics to Publish from this node
     ros::Publisher velocity_publisher = n.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/navi", 1);
@@ -287,54 +262,21 @@ int main(int argc, char **argv)
     //Velocity control variable
     geometry_msgs::Twist vel;
 
-
-    geometry_msgs::Point start_, end_;
-
-    start_.x = 12.0;
-    start_.y = 12.0;
-    start_.z = 0.0;
-    end_.x = -12.0;
-    end_.y = -12.0;
-    end_.z = 0.0;
-
-		//Test points
-		geometry_msgs::Point p1;
-		geometry_msgs::Point p2;
-		geometry_msgs::Point p3;
-		p1.x = 1.0;
-		p1.y = 1.0;
-		p1.z = 0.0;
-		p2.x = 3.0;
-		p2.y = 3.0;
-		p2.z = 0.0;
-		p3.x = 10.0;
-		p3.y = 10.0;
-		p3.z = 0.0;
-		geometry_msgs::Point points[3];
-		points[0] = p1;
-		points[1] = p2;
-		points[2] = p3;
-
-		// gen_milestones();
-		gen_connections(points);
-
-    while (ros::ok())
-    {
+    do {
+    	init_vectors();
+		gen_milestones();			// updates milestones global vector
+		// gen_connections(); 			// updates global Matrix of size numMilestones x numMilestones
+    } while (find_shortest_path());
+	
+    while (ros::ok()) {
     	loop_rate.sleep(); //Maintain the loop rate
     	ros::spinOnce();   //Check for new messages
-
-			//Draw Curves
-			// drawCurve(1);
-			// drawCurve(2);
-			// drawCurve(4);
-	    // drawLineSegment(5,start_,end_);
 
     	//Main loop code goes here:
     	vel.linear.x = 0.1; // set linear speed
     	vel.angular.z = 0.3; // set angular speed
 
     	velocity_publisher.publish(vel); // Publish the command velocity
-
     }
     return 0;
 }
